262259536896e924489fa384289b1fd9fbf21c56
billybu10 <101750525+billybu10@users.noreply.github.com>
2025-06-12 18:37:25 +0200

    Add files via upload

---
+++ inf4.24.06.01/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "inf4.24.06.01"
+include(":app")
+ 
